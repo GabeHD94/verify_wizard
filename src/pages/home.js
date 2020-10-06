d898b4ad8ade3b253d782f523b0ac8f35aeeb617
GabeHD94 <gabehd94@gmail.com>
2020-10-06 13:39:58 -0600

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+ 
+ export default class home extends Component {
+
+
+
+
+     render() {
+         return (
+             <div>
+                 <div className="demobox">
+                     <h1 className="texthead">WIZARD</h1>
+                     <h1 className="texthead">Customer Relations</h1>
+
+                 </div>
+             </div>
+         )
+     }
+ }
+
+ 

509e78347ba3bcd538966899769ca90b2625ea2c
GabeHD94 <gabehd94@gmail.com>
2020-10-06 15:23:44 -0600

    sidebar made

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react'
- 
- export default class home extends Component {
+import React, { Component } from 'react';
+import logo from "../assets/alder-icon-white.png";
+import Homestyle from "../pagestyles/home.css";
+
+export default class home extends Component {
 
 
 
 
-     render() {
-         return (
-             <div>
-                 <div className="demobox">
-                     <h1 className="texthead">WIZARD</h1>
-                     <h1 className="texthead">Customer Relations</h1>
+    render() {
+        return (
+            <div>
+                <div className="demobox">
+                    <img src={logo} alt="logo" />
+                    <h3 className="texthead">Verification Procedure</h3>
+                    <div className="sidebarButtons">
+                        <a className="active" 
+                        href="#home">Home</a>
+                        <a href="#news">News</a>
+                        <a href="#contact">Contact</a>
+                        <a href="#about">About</a>
+                    </div>
+                </div>
+            </div >
+        )
+    }
+}
 
-                 </div>
-             </div>
-         )
-     }
- }
-
- 
